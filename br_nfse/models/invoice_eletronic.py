391ce34c5e72e7e9ce2bd98d42b35499b683bb6a
DiogoBerti <diogoberti88@gmail.com>
2017-05-12 11:02:42 -0300

    Ajustes de erros de RPS e modelo de nfse

--- br_nfse/models/invoice_eletronic.py
+++ br_nfse/models/invoice_eletronic.py
@@ -180,7 +180,10 @@ class InvoiceEletronic(models.Model):
                 'descricao': descricao,
                 'deducoes': [],
             }
-
+            print "=============================="
+            print rps['serie']
+            
+            print "=============================="
             valor_servico = self.valor_final
             valor_deducao = 0.0
 
@@ -192,21 +195,24 @@ class InvoiceEletronic(models.Model):
             codigo_atividade = rps['codigo_atividade']
             tipo_recolhimento = self.operation  # T – Tributado em São Paulo
 
-            assinatura = '%s%s%s%s%sN%s%s%s%s%s%s' % (
+            assinatura = '%s%s%s%s%sN%s%015d%015d%s%s%s' % (
                 str(inscr).zfill(8),
                 self.serie.code.ljust(5),
                 str(self.numero).zfill(12),
                 str(data_envio[0:4] + data_envio[5:7] + data_envio[8:10]),
                 str(tipo_recolhimento),
                 str(iss_retido),
-                str(int(valor_servico*100)).zfill(15),
-                str(int(valor_deducao*100)).zfill(15),
+                round(valor_servico*100),
+                round(valor_deducao*100),
                 str(codigo_atividade).zfill(5),
                 str(tipo_cpfcnpj),
                 str(cnpj_cpf).zfill(14)
                 )
+            
             rps['assinatura'] = assinatura
-
+            print "--------------------------------"
+            print assinatura
+            print "--------------------------------"
             nfse_vals = {
                 'cidade': prestador['cidade'],
                 'cpf_cnpj': prestador['cnpj'],
